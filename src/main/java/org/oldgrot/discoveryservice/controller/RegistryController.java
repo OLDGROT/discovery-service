9f1020aa6fc7b6b936482fbe26eba2345dedebfd
OLDGROT <sashawhite10596@gmail.com>
2025-11-08 12:45:11 +0300

    first

--- src/main/java/org/oldgrot/discoveryservice/controller/RegistryController.java
+++ src/main/java/org/oldgrot/discoveryservice/controller/RegistryController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @AllArgsConstructor
 @RestController()
-@RequestMapping("/discovery-service")
+@RequestMapping()
 public class RegistryController {
     private final RegistryService registryService;
 
